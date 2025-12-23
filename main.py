df31a53f609cd22a2eab324d31542898681d358d
Suzupala <ostrom.j.e@gmail.com>
2025-12-23 23:55:03 +0200

    player class in progress

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import *
+from logger import log_state
+
+
+
+def main():
+	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
+	game_running = True
+	while game_running == True:
+		log_state()
+
+		for event in pygame.event.get():
+    			if event.type == pygame.QUIT:
+        			return
+
+		screen.fill("black")
+		pygame.display.flip()
+		dt = clock.tick(60)/1000
+
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,3 @@
+class Player(CircleShape):
+	def __init__(self, x , y ):
+
