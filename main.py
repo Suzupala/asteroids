d4e26ae051d3e77d7a05a2ef89e937d93aebacd2
Suzupala <ostrom.j.e@gmail.com>
2025-12-25 06:20:42 +0200

    added player and rotation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
-
+from player import *
 
 
 def main():
@@ -13,6 +13,10 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 
+	player_pos_x=SCREEN_WIDTH /2
+	player_pos_y=SCREEN_HEIGHT/2
+	player = Player(player_pos_x,player_pos_y)
+
 	game_running = True
 	while game_running == True:
 		log_state()
@@ -22,9 +26,12 @@ def main():
         			return
 
 		screen.fill("black")
+
+		player.update(dt)
+		player.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
-
 
 if __name__ == "__main__":
     main()
